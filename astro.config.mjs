8b226ff1db66d6f2eede17bd57d71646268d3ca8
Leonardo S. Sousa <33811388+Sousa-L@users.noreply.github.com>
2025-07-15 23:32:52 -0300

    fix: Adapter
    Adds a Node Adapter to Astro.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
+import node from '@astrojs/node';
 
 import react from '@astrojs/react';
 
@@ -10,4 +11,7 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+  adapter: node({
+    mode: 'standalone',
+  }),
 });
